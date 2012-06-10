a973019656ede61d4f001ee850ae24465e3c5858
emil <emil@kjer.info>
2012-06-11 01:04:09 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MexPilotInstructor::Application.config.secret_token = '554a49b650cc7770a1b85825d3391011488cd4e215394d98b0cdbf09db1a9abeb08dbfab67fe1befcb0b8409dc7ad415cd88a95f156cf9746513556126950050'
